bc86d7c2cb67666bc0b72614da22fa5028d02042
Wiki-10 <alealvarez0202@gmail.com>
2021-09-12 11:53:18 -0500

    deleted unused functions

--- main.py
+++ main.py
@@ -14,40 +14,6 @@ def has_malicious(trans, code, palindrome):
     print(False)
 
 
-#Encontrar palindromos
-#######################################
-# Expand in both directions of `low` and `high` to find all palindromes
-def expand(s, low, high, palindromes, origin):
-    # run till `s[low.high]` is not a palindrome
-    while low >= 0 and high < len(s) and s[low] == s[high]:
-        # añade los palindromos a un set y verifica que sean palindromos
-        # mayores a 4 carácteres
-        if(len(s[low: high + 1])>3):
-            current_palindrome = s[low : high + 1]
-            palindromes.add(Palindrome(current_palindrome, low, origin))
-
-        # Expande en ambas direcciones
-        low = low - 1
-        high = high + 1
-
-
-# Función para encontrar todos los palindromos unicos de un string
-def findPalindromicSubstrings(s, file_name):
-    # Crea un set vacio para guardar todos los palindromos únicos
-    palindromes = set()
-
-    for i in range(len(s)):
-
-
-        # Encuentra todos los palindromos impares con s[i] como punto medio
-        expand(s, i, i, palindromes, file_name)
-
-        # Encuentra todos los palindromos pares con s[i] y s[i+1] como punto medio
-        expand(s, i, i + 1, palindromes, file_name)
-
-    # Imprime todos los palindromos unicos
-    return palindromes
-
 def read_file(file_name):
     text_file = open(file_name, "r")
     main_string = text_file.read().replace('\n', '')
