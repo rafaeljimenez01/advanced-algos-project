c06ac1ee2c8bbacdb5864c8df83e1ea7494d2089
Josh Hdz <joshua.hdz.reza@gmail.com>
2021-11-09 13:16:24 -0800

    add generic dijkstra

--- situacion_problema_2/main.py
+++ situacion_problema_2/main.py
@@ -1,3 +1,38 @@
+import heapq
+from typing import Optional
+
+def dijksra(graph, origin):
+    # Dictionary that stores another dictionary to map a node with all other
+    # neighbors and the shortest distance to travel to each other.
+    # (e.g {node: {neighbor_1: dist, neighbor_2: dist, ...}})
+    distances = {node: float('inf') for node in graph}
+    # tuple's list that holds the nodes to be vistied. Each tuple holds the 
+    # distance to the node from the origin and the node to be visited. The order
+    # in which the nodes will be visited depend on the distance value.
+    priority_queue = [(0, origin)]
+    
+    # Distance from  oirign to itlsef set to 0.
+    distances[origin] = 0
+
+    # Iterate through priority queue until empty.
+    while priority_queue:
+        # Pop node with smallest distance.
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        # Only analize node if the distance to current node is not grater.  
+        if current_distance <= distances[current_node]:
+
+            for neighbor, traveled_distance in graph[current_node].items():
+                distance = current_distance + traveled_distance
+
+                # Only consider current path if it's better than any path analyzed
+                # befoire
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances, path
+
 
 # INPUT: file_name -> string.
 # OUTPUT: main_string -> string(file content)
@@ -36,3 +71,6 @@ def fetch_info(file_name):
 
 if __name__ == '__main__':
     neighborhoods_dist, data_cap, station_loc = fetch_info("map1.txt")
+
+    neighborhoods_optimal_dist, optimal_path = dijksra(neighborhoods_dist, 0)
+    a = 1
